7e749ae82e98448ff507798c1b92b3d7916f47ed
Hamdi Boukamcha <contact@matlab-recognition-code.com>
2021-06-27 22:24:29 +0200

    Update sourcecode.m

--- sourcecode.m
+++ sourcecode.m
@@ -5,13 +5,5 @@
 % 
 % http://matlab-recognition-code.com/hand-geometry-recognition-system-matlab-full-source-code/
 %
-% For any question please email me hamdouchhd@hotmail.com
-% 
-% Hamdi Boukamcha
-% Sousse
-% 4081
-% Tunisia
-% email hamdouchhd@hotmail.com
-% mobile +21650674269
 % website http://matlab-recognition-code.com
 %
